5e78abbe8043310aa7c1ce954dd78dd7c5943b05
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-09-08 09:13:42 +0300

    Add type parameter to createBrowserClient

--- packages/auth/client.ts
+++ packages/auth/client.ts
@@ -1,10 +1,11 @@
-"use client";
-
+import type { Database } from "@964reserve/database/types";
 import { createBrowserClient } from "@supabase/ssr";
 
 export const createClient = () => {
-  return createBrowserClient(
+  return createBrowserClient<Database>(
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 };
